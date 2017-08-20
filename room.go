44fdeff1c2ad3a30fd09d3807aaead9b1f5e07de
su-kun1899 <higedrum.coz@gmail.com>
2017-08-20 15:19:00 +0900

    1.2.2 room model

--- room.go
+++ room.go
@@ -1,6 +1,38 @@
 package main
 
 type room struct {
-	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
+	// 他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+	// チャットに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// チャットルームから退室しようとしているクライアントのためのチャネル
+	leave chan *client
+	// 在室している全てのクライアントが保持される
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 入室
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
