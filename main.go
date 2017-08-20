fa2f55c7c3d266986fa1d32a742c47d307a852f4
su-kun1899 <higedrum.coz@gmail.com>
2017-08-20 15:49:39 +0900

    1.2.6 Create chat room

--- main.go
+++ main.go
@@ -22,7 +22,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	//チャットルームを開始
+	go r.run()
 
 	// Starting web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
