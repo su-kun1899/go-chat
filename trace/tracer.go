54ed6990623b9e3d27f1e22255c5dd5308d30321
su-kun1899 <higedrum.coz@gmail.com>
2017-09-02 15:21:18 +0900

    1.4.3 enable to traceoff

--- trace/tracer.go
+++ trace/tracer.go
@@ -21,3 +21,12 @@ func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
 }
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Off はTraceメソッドの呼び出しを無視するTracerを返します
+func Off() Tracer {
+	return &nilTracer{}
+}
